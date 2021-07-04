da6a70452d1ef67310bb72c233ed54b932781ab9
Artemy Voikhansky <artemy@weka.io>
2021-07-04 11:04:47 +0300

    minio: sync object on cluster before sending ack to the user

--- cmd/fs-v1.go
+++ cmd/fs-v1.go
@@ -32,6 +32,7 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
+	"syscall"
 	"time"
 
 	jsoniter "github.com/json-iterator/go"
@@ -1262,6 +1263,10 @@ func (fs *FSObjects) putObject(ctx context.Context, bucket string, object string
 		if _, err = fsMeta.WriteTo(wlk); err != nil {
 			return ObjectInfo{}, toObjectErr(err, bucket, object)
 		}
+	}
+
+	if (GlobalFSOTmpfile) {
+		syscall.Fdatasync(int(file.Fd()))
 	}
 
 	// Stat the file to fetch timestamp, size.
