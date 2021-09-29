243e79980ccf286ebd8b576ca68f8f3f8137ffad
Gershon Geva <gershon@weka.io>
2021-09-29 19:28:33 +0300

    weka : add delete flavors headers (WEKAPP-224156)

--- cmd/bucket-handlers.go
+++ cmd/bucket-handlers.go
@@ -668,7 +668,7 @@ func (api objectAPIHandlers) PutBucketHandler(w http.ResponseWriter, r *http.Req
 				}
 
 				if err = globalDNSConfig.Put(bucket); err != nil {
-					objectAPI.DeleteBucket(ctx, bucket, false)
+					objectAPI.DeleteBucket(ctx, bucket, false, false)
 					writeErrorResponse(ctx, w, toAPIError(ctx, err), r.URL, guessIsBrowserReq(r))
 					return
 				}
@@ -1078,10 +1078,15 @@ func (api objectAPIHandlers) DeleteBucketHandler(w http.ResponseWriter, r *http.
 		}
 	}
 
+	unlinkBucket := false
+	if _, found := r.Header[http.CanonicalHeaderKey("x-weka-unlink-bucket")]; found {
+		unlinkBucket = true
+	}
+
 	deleteBucket := objectAPI.DeleteBucket
 
 	// Attempt to delete bucket.
-	if err := deleteBucket(ctx, bucket, forceDelete); err != nil {
+	if err := deleteBucket(ctx, bucket, forceDelete, unlinkBucket); err != nil {
 		if _, ok := err.(BucketNotEmpty); ok && (globalBucketVersioningSys.Enabled(bucket) || globalBucketVersioningSys.Suspended(bucket)) {
 			apiErr := toAPIError(ctx, err)
 			apiErr.Description = "The bucket you tried to delete is not empty. You must delete all versions in the bucket."
