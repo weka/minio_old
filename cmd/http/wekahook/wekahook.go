ef89b87e139280e0981850a2b26d23f19613805f
Moses Reuben <moses@weka.io>
2021-11-08 13:15:26 +0200

    weka : adding NDU support (WEKAPP-223429) - add support for tls connection

--- cmd/http/wekahook/wekahook.go
+++ cmd/http/wekahook/wekahook.go
@@ -18,28 +18,51 @@ type WResponseWriter struct {
 }
 
 func (w WResponseWriter) Flush() {
-	if flush, ok := w.ResponseWriter.(http.Flusher) ; ok {
+	if flush, ok := w.ResponseWriter.(http.Flusher); ok {
 		flush.Flush()
 	}
 }
 
-func (w WResponseWriter) WriteHeader(statusCode int ) {
+func Maze(obj interface{}) {
+	typeObj := reflect.TypeOf(obj)
+	if typeObj.Kind() == reflect.Ptr {
+		typeObj = typeObj.Elem()
+	}
+	fmt.Printf("Maze? %T kind of %v\n", obj , typeObj.Kind() )
+	if typeObj.Kind() == reflect.Struct {
+		for i := 0; i < typeObj.NumField(); i++ {
+			fmt.Printf("Maze? \t %d - %s: %v\n", i, typeObj.Field(i).Name, typeObj.Field(i).Type)
+		}
+	}
+}
+
+
+func (w WResponseWriter) WriteHeader(statusCode int) {
 	pw := w.ResponseWriter
+
 	httpCom := reflect.ValueOf(pw).Elem().Field(0) // We know ResponseWriter is http.response ( private structure)
-													 // we took the field 0 which is a netcom object
+
+
+
 	netComPrivate := httpCom.Elem().Field(2) // the field num 2 is a http com object
 	PointerNetCom := netComPrivate.UnsafeAddr()
-	netCon := reflect.NewAt(
-		netComPrivate.Type(),
-		unsafe.Pointer(PointerNetCom)).Elem().Interface().(*net.TCPConn) // We can't access this object private
-																			// so we create an object of the same type at the same memory address
-	netCon.Write([]byte("HTTP/1.1 "))
-	fmt.Printf("%T -> %v ->  %v -> %T\n", pw , httpCom.Type(), netComPrivate, netCon)
 
-	for *w.UpgradeMode {
-		netCon.Write([]byte(" "))
-		time.Sleep(1 * time.Second)
+	tmpObj := reflect.NewAt(netComPrivate.Type(), unsafe.Pointer(PointerNetCom)).Elem().Interface()
+	Maze(tmpObj)
+	netCom := tmpObj.(net.Conn)
+
+	netCom.Write([]byte("HTTP/1.1 "))
+	fmt.Printf("%T -> %v ->  %v -> %T\n", pw, httpCom.Type(), netComPrivate, netCom)
+
+	if false {
+		w.ResponseWriter.WriteHeader(http.StatusGatewayTimeout)
+	} else {
+		for *w.UpgradeMode {
+			netCom.Write([]byte(" "))
+			time.Sleep(1 * time.Second)
+		}
 	}
-	netCon.Write([]byte(fmt.Sprintf("%d %s\nProtocol: ",statusCode, http.StatusText(statusCode))))
+
+	netCom.Write([]byte(fmt.Sprintf("%d %s\nProtocol: ", statusCode, http.StatusText(statusCode))))
 	w.ResponseWriter.WriteHeader(statusCode)
 }
